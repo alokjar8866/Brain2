3a50f6e19690b4fe16d85aee6144f483722f956a
Alok Yadav <alokjar212@gmail.com>
2026-02-06 13:03:33 +0530

    Update:Input Component is done

--- Frontend/Brain-Frontend/src/components/Input.tsx
+++ Frontend/Brain-Frontend/src/components/Input.tsx
@@ -1,5 +1,14 @@
-export function Input({onChange, placeholder}:{placeholder:string; onChange:()=>void}){
+export function Input({ onChange, placeholder }: { placeholder: string; onChange?: () => void }) {
     return <div>
-        <input placeholder={placeholder} type="text" className="m-2 border-2 border-black rounded px-4 py-2" onChange={onChange} />
+        <input
+        onChange={onChange}
+        type="text"
+        placeholder={placeholder}
+        className="w-60 h-10 max-w-md px-4 py-2 bg-zinc-600 border-2 border-black rounded-md 
+                   text-white placeholder-white
+                   focus:outline-none focus:ring-2 focus:ring-blue-700 focus:border-gray-400 
+                   transition-all duration-200 ease-in-out"
+      />
     </div>
+        
 }
