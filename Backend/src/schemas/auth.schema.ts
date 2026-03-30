5475e4547e1d71b58d9babb7979249393ad7e985
Alok Yadav <alokjar212@gmail.com>
2026-03-30 23:58:12 +0530

    feat:added schema for validation of input

---
+++ Backend/src/schemas/auth.schema.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const registerSchema = z.object({
+  fullName: z.string().min(2, "Full name must be at least 2 characters"),
+  username: z.string().min(3, "Username must be at least 3 characters"),
+  password: z.string().min(6, "Password must be at least 6 characters"),
+});
+
+// Infering the type so it can be used for typing req.body
+export type RegisterInput = z.infer<typeof registerSchema>;
