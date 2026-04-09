2d58f35f775592947269d03643b3bde86e3a22b7
Alok Yadav <alokjar212@gmail.com>
2026-04-09 14:49:14 +0530

    some style changes in navbar component

--- Frontend/Brain-Frontend/src/components/Navbar.tsx
+++ Frontend/Brain-Frontend/src/components/Navbar.tsx
@@ -13,7 +13,7 @@ function Navbar() {
       </div>
       <div className="flex gap-9 items-center">
         {["Features", "How it Works", "FAQ"].map(item => (
-          <a key={item} href="#" className="text-[#9994b8] no-underline text-sm font-['Syne',sans-serif] font-medium tracking-[0.02em] transition-colors duration-200 hover:text-[#1a69fc]">{item}</a>
+          <a key={item} href="#" className="text-[#9994b8] no-underline text-sm font-['Syne',sans-serif]  font-bold tracking-[0.02em] transition-colors duration-200 hover:text-[#1a69fc]">{item}</a>
         ))}
         <button className="bg-[#4583f5] text-[#07070b] font-syne font-bold text-[12px] tracking-[0.04em] border-none cursor-pointer py-2 px-7 rounded-full transition-all duration-200 hover:bg-[#4583f5] hover:-translate-y-px hover:shadow-[0_0_30px_rgba(200,245,69,0.3)]">
           Get Started Here!
