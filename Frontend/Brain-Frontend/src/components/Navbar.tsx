ecfd012a482d951180e452d6746df847da2cacc5
Alok Yadav <alokjar212@gmail.com>
2026-05-20 23:47:27 +0530

    fix:added navigation in get started button

--- Frontend/Brain-Frontend/src/components/Navbar.tsx
+++ Frontend/Brain-Frontend/src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 
+import { Link } from 'react-router-dom';
 import { BrainLogo } from '../icons/BrainLogo';
 
 
@@ -16,9 +17,9 @@ function Navbar() {
           <a key={item} href="#" className="text-[#9994b8] no-underline font-sans text-sm font-bold tracking-[0.02em] transition-colors duration-200 hover:text-[#0e5ef3]">{item}</a>
         ))}
        <button className="group relative bg-[#1549e6] hover:bg-[#275dff] text-white font-syne font-bold text-sm tracking-widest uppercase px-5 py-2 rounded-full hover:shadow-[#3088fc]/50 hover:-translate-y-0.5 active:translate-y-0 active:scale-95 transition-all duration-200 cursor-pointer border-none">
-    <span className="flex items-center gap-2">
+    <Link to="/signin" className="flex items-center gap-2">
         Get Started
-    </span>
+    </Link>
 </button>
       </div>
     </nav>
