08b37db57be32c9cb1ebd4dfb0185486290d3e09
Alok Yadav <alokjar212@gmail.com>
2026-05-25 23:58:30 +0530

    fix:compiled all components on landing page

--- Frontend/Brain-Frontend/src/App.tsx
+++ Frontend/Brain-Frontend/src/App.tsx
@@ -1,16 +1,22 @@
-import { BrowserRouter,Route,Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { SignUp } from "./pages/SignUp"
 import { SignIn } from "./pages/SignIn"
 import { Dashboard } from "./pages/Dashboard"
-
+import PreLanding from "./pages/PreLanding"
+import LandingPage from "./pages/LandingPage"
+import { Toaster } from "sonner"
+import { SharedBrainPage } from "./pages/SharedBrainPage"
 
 function App() {
-
   return <BrowserRouter>
+    <Toaster position="bottom-right" richColors duration={3000} theme="system" />
     <Routes>
-      <Route path="/signup" element={<SignUp/>} />
-      <Route path="/signin" element={<SignIn/>} />
-      <Route path="/dashboard" element={<Dashboard/>} />
+      <Route path="/home" element={<PreLanding />} />
+      <Route path="/home2" element={<LandingPage />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/brain/:shareLink" element={<SharedBrainPage />} />
     </Routes>
   </BrowserRouter>
 }
