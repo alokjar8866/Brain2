6efc3e09856faba558f7a3c650a905e095b3ad20
Alok Yadav <alokjar212@gmail.com>
2026-09-20 22:33:22 +0530

    added the cors origin and Backend general url

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -12,7 +12,7 @@ const app:express.Application = express();
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin:process.env.FRONTEND_URL }));
 
 connectDB();
 
