49c21cfbf26dbf8dc83f574241d91830a9b2f9da
Alok Yadav <alokjar212@gmail.com>
2026-09-22 01:14:10 +0530

    fixed the home page redirection

--- Frontend/Brain-Frontend/src/App.tsx
+++ Frontend/Brain-Frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import { SignUp } from "./pages/SignUp"
 import { SignIn } from "./pages/SignIn"
 import { Dashboard } from "./pages/Dashboard"
@@ -10,7 +10,8 @@ function App() {
   return <BrowserRouter>
     <Toaster position="bottom-right" richColors duration={3000} theme="system" />
     <Routes>
-      <Route path="/home2" element={<LandingPage />} />
+      <Route path="/" element={<Navigate to="/home" replace/>} />
+      <Route path="/home" element={<LandingPage />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/signin" element={<SignIn />} />
       <Route path="/dashboard" element={<Dashboard />} />
