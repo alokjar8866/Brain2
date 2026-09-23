1a9988175b3ad727d464bebb67ab7b2f46c19e45
Alok Yadav <alokjar212@gmail.com>
2026-09-23 23:39:57 +0530

    fix:share content route

--- Frontend/Brain-Frontend/src/config.ts
+++ Frontend/Brain-Frontend/src/config.ts
@@ -1,3 +1,3 @@
 //export const BACKEND_URL="http://localhost:3000";
 export const BACKEND_URL= import.meta.env.VITE_API_URL;
-export const FRONTEND_URL = import.meta.env.FRONTEND_URL
+export const FRONTEND_URL = import.meta.env.VITE_FRONTEND_URL
